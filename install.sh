1693dad33d87481dd0cae9d25adf0eaffbb94cd0
codyborn <codyborn@outlook.com>
2019-11-26 23:05:16 -0800

    Use .desktop file to auto start on raspi

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get install chromium-browser --yes
 
 # hide cursor
 sudo apt-get install unclutter
-sudo sed -i -e '$i @unclutter -idle 0.1' /etc/rc.local
+sudo echo "@unclutter -idle 0.1" >> /etc/rc.local
 
 # generate unique id
 sudo apt-get install --reinstall wamerican
@@ -14,6 +14,13 @@ frameid=$(shuf -n4 /usr/share/dict/words | tr '\n' ' ' | tr -d "'s" | tr '[:uppe
 echo $frameid >> frameid.txt
 
 # set chromium to open page on start up
-sudo sed -i -e '$i chromium-browser --start-fullscreen https://tokencast.net/device?deviceId=$frameid &\' /etc/rc.local
+mkdir /home/pi/.config/autostart
+
+echo "[Desktop Entry]
+Type=Application
+Name=TokenCast
+Exec=chromium-browser --start-fullscreen https://tokencast.net/device?deviceId=$frameid" >> /home/pi/.config/autostart/tokencast.desktop
+
+
 chromium-browser --start-fullscreen https://tokencast.net/device?deviceId=$frameid
 
