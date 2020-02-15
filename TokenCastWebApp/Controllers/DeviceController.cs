b45c7dcab87a17678264e0177a02a4fb6ca3b32f
codyborn <codyborn@outlook.com>
2020-02-15 11:00:23 -0800

    QR code generation in controller

--- TokenCastWebApp/Controllers/DeviceController.cs
+++ TokenCastWebApp/Controllers/DeviceController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QRCoder;
 using TokenCast.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -39,6 +42,20 @@ namespace TokenCast.Controllers
             }
 
             return lastUpdated.time.Ticks;
+        }
+
+        public ActionResult GetQRCode(string url)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.Transparent, drawQuietZones: true);
+            using (var stream = new MemoryStream())
+            {
+                qrCodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                Byte[] imageOut = stream.ToArray();
+                return File(imageOut, "image/png");
+            }
         }
     }
 }
