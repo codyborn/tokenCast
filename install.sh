cc6bacbf98bffa7b9d8f13bdd4e55a5527a62d16
Cody Born <codyborn@outlook.com>
2022-03-15 14:49:33 -0700

    Fix raspi installation script
    These config settings are no longer compatible with the latest version of raspbian.

--- install.sh
+++ install.sh
@@ -15,12 +15,6 @@ xserver-command=X -s 0 -dpms" >> /etc/lightdm/lightdm.conf
 # Remove temperature warning icon
 sudo echo "avoid_warnings=1" >> /boot/config.txt
 
-# Run with Fake KMS driver (openGL drivers)
-sudo echo "dtoverlay=vc4-fkms-v3d" >> /boot/config.txt
-
-# Max out memory for GPU
-sudo echo "gpu_mem=512" >> /boot/config.txt
-
 # generate unique id
 sudo apt-get install --reinstall wamerican
 frameid=$(shuf -n4 /usr/share/dict/words | tr '\n' '_' | tr -d "'" | tr '[:upper:]' '[:lower:]')
