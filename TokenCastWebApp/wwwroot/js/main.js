f2ef194b185433b4cffe8fe587147322e1ee0b73
VadyChel <67959852+VadyChel@users.noreply.github.com>
2023-01-26 23:35:37 +0200

    Fix device statuses (#13)
    * @ -> v-on:
    
    * fix search and fix titles styles
    
    * replace %wallet_id% to real value
    
    * fix content margin top
    
    * fix nft-container styles
    
    * fix device adding
    
    * fix
    
    * remove 'walletconnect' key from localstorage after logout
    
    * fix get tokens when devices not found
    
    * fix sidebar styles
    
    * adaptive design
    
    * more cast token settings
    
    * fix updating and adding devices
    
    * add online/offline statuses
    
    * new walletbeacon
    
    * change init place for device statuses ws
    
    * fix get community tokens

--- TokenCastWebApp/wwwroot/js/main.js
+++ TokenCastWebApp/wwwroot/js/main.js
@@ -190,10 +190,32 @@ window.addEventListener('load', async () => {
   getLastUsedDevice();
 }, false);
 
+async function connectDeviceStatusesWS() {
+  const resp = await fetch('/connect/ui/create', { method: 'POST' })
+  const { connectionId } = await resp.json()
+  const deviceStatusesSocket = new WebSocket(
+    `wss://nftframe.azurewebsites.net/connect/ui?connectionId=${connectionId}&address=${app.address}`
+  );
+
+  deviceStatusesSocket.onmessage = (event) => {
+    console.log("device statuses event", event.data)
+    const data = JSON.parse(event.data)
+
+    if (data.event === 'Online') {
+      app.deviceStatuses[data.deviceId] = true
+    }
+
+    if (data.event === 'Offline') {
+      app.deviceStatuses[data.deviceId] = false
+    }
+  }
+}
+
 async function initEthereumAccount() {
   app.providedWeb3 = new Web3(provider);
   web3Account = (await app.providedWeb3.eth.getAccounts())[0];
   app.address = web3Account;
+  await connectDeviceStatusesWS();
   await AttemptReverse(web3Account);
 }
 
@@ -211,6 +233,7 @@ async function initTezosAccount(onConnect) {
     }
     if (activeAccount) {
       app.address = activeAccount.address;
+      await connectDeviceStatusesWS();
       web3Account = app.address;
     }
   } catch (error) {
@@ -487,20 +510,21 @@ async function GetTokens() {
   await GetCommunityTokens()
 }
 
-  async function GetCommunityTokens() {
-
+async function GetCommunityTokens() {
   app.showFetchingTokensMessage = true;
 
   $.get(origURL + "Account/CommunityTokens", function (tokenResponse) {
-  var parsedTokens = JSON.parse(tokenResponse);
-  app.showFetchingTokensMessage = false;
-  app.communityTokens = [];
-  parsedTokens.assets.forEach(function (token) {
-  if (token.image_url !== "") {
-  app.communityTokens.push(token);
-}
-})
-});
+    var parsedTokens = JSON.parse(tokenResponse);
+    app.showFetchingTokensMessage = false;
+    app.communityTokens = [];
+    parsedTokens.assets.forEach(function (token) {
+      if (token.image_url !== "") {
+        app.communityTokens.push(token);
+      }
+    })
+
+    app.tokens.push(...app.communityTokens)
+  });
 }
 
   function CopyToClipboard(str) {
